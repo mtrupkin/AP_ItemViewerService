33e17eb044f0ca1659d8e6278b243a5b1720adfe
Garrett Smith <garrett.smith@oregonstate.edu>
2017-02-08 14:06:38 -0800

    Update the deploy script to work with dev branch

--- DeployScripts/runEcsBuild.sh
+++ DeployScripts/runEcsBuild.sh
@@ -2,7 +2,7 @@
 set -ev
 
 if [ "$BRANCH" == "dev" ]; then
-    aws ecs run-task --region us-west-2 --cluster SampleBuilder --count 1 --task-definition ItemViewerServiceBuilder-stage
+    aws ecs run-task --region us-west-2 --cluster SampleBuilder --count 1 --task-definition ItemViewerServiceBuilder-dev
 fi
 
 if [ "$BRANCH" == "master" ]; then

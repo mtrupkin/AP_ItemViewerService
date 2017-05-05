bb7fa31d07ac5b78840858f4c6b4e590ec611eb9
Garrett Smith <garrett.smith@oregonstate.edu>
2017-05-05 13:30:11 -0700

    Updated docker config
    Travis will now build the complete app

--- DeployScripts/runEcsBuild.sh
+++ DeployScripts/runEcsBuild.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
 set -ev
 
-if [ "$BRANCH" == "dev" ]; then
-    aws ecs run-task --region us-west-2 --cluster SampleBuilder --count 1 --task-definition ItemViewerServiceBuilder-dev
-fi
+#Do nothing if this is a pull request.
+# Pull request is either false, or the PR number if it is a PR.
+if [[ ! ( "$TRAVIS_PULL_REQUEST" == "false" ) ]]; then exit 0; fi
 
-if [ "$BRANCH" == "master" ]; then
-    aws ecs run-task --region us-west-2 --cluster SampleBuilder --count 1 --task-definition ItemViewerServiceBuilder-prod
+Region="us-west-2"
+aws ecr get-login --region $Region | source /dev/stdin
+
+if ( [ "$BRANCH" == "feature_travis-docker" ] || [ "$BRANCH" == "dev" ] || [ "$BRANCH" == "master" ] ) && [ "$TRAVIS_PULL_REQUEST" == "false" ]
+then
+    ServiceName="ItemViewerService-dev"
+    if [ "$BRANCH" == "dev" ]
+    then
+        ServiceName="ItemViewerService-dev"
+        ClusterName="ItemViewerService-prod"
+    elif [ "$BRANCH" == "master" ]
+    then
+        ServiceName="ItemViewerService-prod"
+        ClusterName="ItemViewerService-prod"
+    elif [ "$BRANCH" == "feature_travis-docker" ]  # For testing
+    then
+        ServiceName="ItemViewerService-dev"
+        ClusterName="ItemViewerService-prod"
+    fi
+
+    mkdir content
+    wget https://s3-us-west-2.amazonaws.com/cass-sb-itemviewerservice-content/content.zip
+    unzip -o content.zip -d content/ &> /dev/null
+    rm content.zip 
+    docker build -t itemviewerserviceapp .
+    docker tag itemviewerserviceapp:latest osucass/itemviewerserviceapp:$BRANCH
+    docker push osucass/itemviewerserviceapp:$BRANCH
+    ecs deploy $ClusterName $ServiceName --region $Region
 fi

2db4e68bfc847709dfdc3ee369d4aebaa0a58f2d
alexlepinski <alex.lepinski@oregonstate.edu>
2017-05-11 14:24:45 -0700

    added docker travis deploy

--- DeployScripts/app_docker_deploy.sh
+++ DeployScripts/app_docker_deploy.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -ev
 
-ServiceName=$1
-ClusterName=$2
-
-Region="us-west-2"
-aws ecr get-login --region $Region | source /dev/stdin
-
 cd docker/
 mkdir content
 wget "$CONTENT_PACKAGE_URL" -O content.zip
@@ -15,4 +9,3 @@ rm content.zip
 docker build -t itemviewerserviceapp .
 docker tag itemviewerserviceapp:latest osucass/itemviewerserviceapp:$BRANCH
 docker push osucass/itemviewerserviceapp:$BRANCH
-ecs deploy $ClusterName $ServiceName --region $Region --timeout 600
